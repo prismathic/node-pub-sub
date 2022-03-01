12b68a1e312113ea642d44f5085399c825ddf2d8
prismathic <pjjsalam1999@gmail.com>
2022-03-01 07:09:44 +0100

    update eslint config

---
+++ src/app.ts
@@ -0,0 +1,22 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import path from 'path';
+
+// Create Express server
+const app: express.Application = express();
+
+// Express configuration
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, '../views'));
+app.set('view engine', 'ejs');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(
+  (req: express.Request, res: express.Response, next: express.NextFunction) => {
+    res.locals.user = req.user;
+    next();
+  },
+);
+
+export default app;
--- src/index.ts
+++
@@ -1,7 +0,0 @@
-console.log("My first TypeScript program");
-
-const a = 5
-
-if (a === 5) {
-    console.log("a is 5")
-}
